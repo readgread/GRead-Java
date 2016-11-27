14723cf192feb6ffa0803a2d7631dde07b4450ab
mrishan1993 <mrishan1993@gmail.com>
2016-11-27 21:20:29 +0530

    Added icons in action bar

---
+++ app/src/main/java/com/example/gread/gread/NavDrawerItem.java
@@ -0,0 +1,16 @@
+package com.example.gread.gread;
+
+/**
+ * Created by ISHAN_GUPTA on 11/22/2016.
+ */
+public class NavDrawerItem
+{
+    public int icon;
+    public String name;
+
+    public NavDrawerItem(int icon, String name)
+    {
+        this.icon = icon;
+        this.name = name;
+    }
+}
